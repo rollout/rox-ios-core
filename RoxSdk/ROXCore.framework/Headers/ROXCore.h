e2a7c76efc0cb39a1dacba41f2b0ef78c48353f0
rollout-release <rollout-release@users.noreply.github.com>
2021-03-08 01:48:47 +0000

    version 4.10.8, build 543

--- RoxSdk/ROXCore.framework/Headers/ROXCore.h
+++ RoxSdk/ROXCore.framework/Headers/ROXCore.h
@@ -1,5 +1,5 @@
 //
-//  ROXCore SDK version 4.10.7, Build 542
+//  ROXCore SDK version 4.10.8, Build 543
 //
 //  Copyright (c) 2017 rollout.io. All rights reserved.
 //
--- RoxSdk/RoxSdk/RoxSdk.h
+++ RoxSdk/RoxSdk/RoxSdk.h
@@ -1,5 +1,5 @@
 //
-//  ROX SDK version 4.10.7, Build 542
+//  ROX SDK version 4.10.8, Build 543
 //
 //  Copyright (c) 2017 rollout.io. All rights reserved.
 //
