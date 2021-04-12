f7c3f2ad9eb41eaabf7d0bb101652c97a729025e
rollout-release <rollout-release@users.noreply.github.com>
2021-04-12 17:25:57 +0000

    version 4.10.11, build 548

--- RoxSdk/ROXCore.framework/Headers/ROXCore.h
+++ RoxSdk/ROXCore.framework/Headers/ROXCore.h
@@ -1,5 +1,5 @@
 //
-//  ROXCore SDK version 4.10.10, Build 546
+//  ROXCore SDK version 4.10.11, Build 548
 //
 //  Copyright (c) 2017 rollout.io. All rights reserved.
 //
--- RoxSdk/RoxSdk/RoxSdk.h
+++ RoxSdk/RoxSdk/RoxSdk.h
@@ -1,5 +1,5 @@
 //
-//  ROX SDK version 4.10.10, Build 546
+//  ROX SDK version 4.10.11, Build 548
 //
 //  Copyright (c) 2017 rollout.io. All rights reserved.
 //
