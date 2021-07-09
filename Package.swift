9447a6dc8a44f15bc9c6f698c948ce964e50798e
Arnaud Dorgans <arnaud.dorgans@backmarket.com>
2021-07-09 10:23:47 +0200

    Merged SPM

--- Package.swift
+++ Package.swift
@@ -4,13 +4,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "ROXCore",
+  name: "ROX",
   platforms: [.iOS(.v8), .tvOS(.v10)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "ROXCore",
       targets: ["ROXCore"]),
+    .library(
+      name: "ROX",
+      targets: ["ROX"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,13 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .binaryTarget(name: "ROXCore",
-                  path: "ROXCore.xcframework")
+                  path: "ROXCore.xcframework"),
+    .target(
+      name: "ROX",
+      dependencies: ["ROXCore"],
+      path: "RoxSdk/RoxSdk",
+      exclude: [
+        "Info.plist",
+      ])
   ]
 )
